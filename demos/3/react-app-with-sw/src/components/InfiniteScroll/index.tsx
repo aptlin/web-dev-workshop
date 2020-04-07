7cae9a60b7998180f5846a6cfd5f7eda4938a5ef
Sasha Illarionov <sasha@sdll.io>
2020-04-07 13:57:32 +0300

    fix: add throttling period

--- demos/3/react-app-with-sw/src/components/InfiniteScroll/index.tsx
+++ demos/3/react-app-with-sw/src/components/InfiniteScroll/index.tsx
@@ -1,27 +1,33 @@
 import React, { useEffect } from 'react';
 import config from '../../config';
+import throttle from 'lodash.throttle';
 
 export interface InfiniteScrollProps {
   onFetchMore?: () => void;
   activationDistance?: number;
+  throttlingPeriod?: number;
 }
 
 const InfiniteScroll: React.FC<InfiniteScrollProps> = ({
   children,
   onFetchMore = () => {},
   activationDistance = config.constants.ACTIVATION_DISTANCE,
+  throttlingPeriod = config.constants.THROTTLING_PERIOD,
 }) => {
   useEffect(() => {
-    const onScroll = () => {
+    const onScroll = throttle(() => {
       const documentHeight = document.body.offsetHeight;
       const scrollHeight = window.innerHeight + window.scrollY;
 
       if (scrollHeight + activationDistance >= documentHeight) {
         onFetchMore();
       }
-    };
+    }, throttlingPeriod);
+
     window.addEventListener('scroll', onScroll, false);
+
     return () => window.removeEventListener('scroll', onScroll, false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return <div>{children}</div>;
 };
