4487a2f9967496a8778a4c1c88f35e52e8022474
Sasha Illarionov <sasha@sdll.io>
2020-04-10 19:49:23 +0300

    fix: 404 page

--- demos/3/react-app-with-sw/src/pages/NotFound/index.tsx
+++ demos/3/react-app-with-sw/src/pages/NotFound/index.tsx
@@ -1,6 +1,6 @@
 import * as Sentry from '@sentry/browser';
 import React, { useEffect, useState } from 'react';
-import { Container, Jumbotron } from 'reactstrap';
+import { Container, Jumbotron, Row, Col } from 'reactstrap';
 import Head from '../../components/Head';
 import Header from '../../components/Header';
 import Suggestions from '../../containers/Suggestions';
@@ -29,11 +29,21 @@ const NotFound: React.FC = () => {
     <Container>
       <Head pageTitle={'Page not found'} />
       <Header />
-      <Jumbotron className="bg-white my-0 py-0">
-        <h1>Alas, the page you are looking for does not exist.</h1>
-        <Suggestions className="flex-row flex-wrap" horizontal flush={false} />
-        {image ? <GalleryItem experience={image} isCompressed={false} /> : null}
-      </Jumbotron>
+      <h1 className="text-danger px-4 pb-4">
+        Alas, the page you are looking for does not exist.
+      </h1>
+      <Row>
+        <Col>
+          <Suggestions />
+        </Col>
+        <Col>
+          <Jumbotron>
+            {image ? (
+              <GalleryItem experience={image} isCompressed={false} />
+            ) : null}
+          </Jumbotron>
+        </Col>
+      </Row>
     </Container>
   );
 };
